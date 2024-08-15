a59c7115f6ac2aa5f585b6ef1a2384bfcd75e0d4
akshatbhansali <akshatb2198@gmail.com>
2024-08-16 01:29:44 +0530

    added cart onlclick

--- src/components/ThreeD/Furniture.jsx
+++ src/components/ThreeD/Furniture.jsx
@@ -268,7 +268,7 @@ const Furniture = ({ onLaptopClick }) => {
         onPointerOver={handlePointerOver}
         onPointerOut={handlePointerOut}
       >
-        <group>
+        <group onClick={() => handleLaptopClick("cart")}>
           <Cart1 />
           <Cart2 />
           <Cart3 />
