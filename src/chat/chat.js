82d6aec6f23876569ca0e79e6fbdeda363ca9451
akshitnandan <akshitnandan@gmail.com>
2024-08-15 16:16:44 +0530

    fixed prompt

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -8,7 +8,8 @@ function initializeModel() {
     model: "gemini-1.5-flash",
     systemInstruction: `"You are an AI store helper for an electronics gadget store Metamart. Your primary role is to suggest gadgets from the available gadgets list provided to you. Make sure to suggest a gadget most of the time, but you can choose not to suggest anything if appropriate. Your response should include an 'id' of the suggested gadget and a 'reply' with a text message. If you decide not to suggest any gadget, set 'id' to null and provide a suitable response in 'reply'.
     Format of reply -> {"id":111,"reply":"sdfsfsdf" }. It should be directly parseable by JSON. Don't put json word in response.
-    Here is the data of available gadgets ${JSON.stringify(data)}.`,
+    Here is the data of available gadgets ${JSON.stringify(data)}.
+    Also while suggesting any product pick exactly one of the gadget from the data and give exactly id of that data.`,
     generationConfig: {
       maxOutputTokens: 350,
     },
