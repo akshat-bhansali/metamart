4096dbffb3935ae8b1b1def80f6c4a62bac6b5f9
Rohan Prakash <pksh.rohan16@gmail.com>
2025-07-29 20:17:21 +0530

    [FIX] Code modularity

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push('metaverse')
+    router.push('metaverse') //go to metaverse page
   }, []);
   return (
       <div></div>
